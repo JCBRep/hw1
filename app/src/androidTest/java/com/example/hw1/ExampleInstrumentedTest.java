f7709c78ddbce52ada77223963be2be9366c8665
Jorge <calvoblancojorge@gmail.com>
2019-10-03 13:43:31 +0200

    Primer commit

---
+++ app/src/androidTest/java/com/example/hw1/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.example.hw1;
+
+import android.content.Context;
+
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.example.hw1", appContext.getPackageName());
+    }
+}
